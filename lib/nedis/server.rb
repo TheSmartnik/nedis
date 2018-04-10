9480c00d2bd882ada864040eb2812a28fc89697d
 TheSmartnik <misharinn@gmail.com>
2018-04-10 17:43:57 +0300

    Add buffer support

--- lib/nedis/server.rb
+++ lib/nedis/server.rb
@@ -23,17 +23,24 @@ module Nedis
             child_socket = socket.accept
             clients[child_socket] = Handler.new(child_socket)
           else
-            clients[socket].proccess!
+            begin
+              clients[socket].proccess!
+            rescue EOFError
+              clients.delete(socket)
+              socket.close
+            end
           end
         end
       end
     ensure
-      readable.each(&:close)
+      (readable + clients.keys).each(&:close)
     end
   end
 
   class Handler
-    attr_reader :client
+    ProtocolError = Class.new(RuntimeError)
+
+    attr_reader :client, :buffer
 
     def initialize(socket)
       @client = socket
@@ -41,26 +48,62 @@ module Nedis
     end
 
     def proccess!
-      header = client.gets.to_s
+      buffer << client.read_nonblock(10)
+      cmds, processed = unmarshal(buffer)
+      @buffer = buffer[processed..-1]
 
-      return unless header[0] == '*'
+      cmds.each do |cmd|
+        response = case cmd[0].downcase
+        when 'ping' then"+PONG\r\n"
+        when 'echo' then "$#{cmd[1].length}\r\n#{cmd[1]}\r\n"
+        end
 
-      arguments_count = header[1..-1].to_i
+        client.write response
+      end
+    end
 
-      cmd = arguments_count.times.map do
-        len = client.gets[1..-1].to_i
-        carriage_return_length = 2
-        client.read(len + carriage_return_length).chomp
+
+    def unmarshal(data)
+      io = StringIO.new(data)
+      result = []
+      processed = 0
+
+      begin
+        loop do
+          header = safe_readline(io)
+          raise ProtocolError if header[0] != '*'
+          n = header[1..-1].to_i
+
+          result << n.times.map do
+            raise ProtocolError if io.readpartial(1) != '$'
+
+            length = safe_readline(io).to_i
+            safe_readpartial(io, length).tap do
+              safe_readline(io)
+            end
+          end
+
+          processed = io.pos
+        end
+      rescue ProtocolError
+        processed = io.pos
+      rescue EOFError
+        # Do nothing for now
       end
 
-      command, *cmd_arguments = cmd
+      [result, processed]
+    end
 
-      response = case command.downcase
-      when 'ping' then"+PONG\r\n"
-      when 'echo' then "$#{cmd_arguments[0].length}\r\n#{cmd_arguments[0]}\r\n"
+    def safe_readline(io)
+      io.readline("\r\n").tap do |line|
+        raise EOFError unless line.end_with?("\r\n")
       end
+    end
 
-      client.write response
+    def safe_readpartial(io, length)
+      io.readpartial(length).tap do |line|
+        raise EOFError if line.length != length
+      end
     end
   end
 end
